e97facfd6921787e3d4dd68e49df2ad71669ba48
PedroGomes <pedrogomeslycee@gmail.com>
2026-04-29 21:57:55 +0200

    chore: update server to bind to 0.0.0.0 and simplify startup log message

--- server/server.js
+++ server/server.js
@@ -53,6 +53,6 @@ app.use("/api/keepAlive/Health", async (req, res) => {
   }
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port: ${port}`);
 });
